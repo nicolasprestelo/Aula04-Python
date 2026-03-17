94fa2e0f1efc586862634d2c1a87a1b40d073067
Nicolas Prestelo da Rocha <nicolasprestelorocha@gmail.com>
2026-03-16 23:11:43 -0300

    Conclusão Exercicio10.py

--- Exercicio10.py
+++ Exercicio10.py
@@ -1,11 +1,20 @@
-numeroBase = int(input("Informe um número com 3 dígitos: "))
+numeroPedido = int(input("Digite um número inteiro com três dígitos: "))
 
-primeiroNumero = numeroBase // 100
-resto = numeroBase % 100
-segundoNumero = resto // 10
-resto2 = resto % 10
+unidade = numeroPedido % 10
+# Pega o resto da divisão por 10
+# ex: 123 / 10 =  12.3 ---> 12 dezenas inteiras com resto 3
+# logo o valor é o resto, 3
+
+dezena = (numeroPedido // 10 ) % 10
+#Pega o resultado inteiro da divisao por 10 e pega o resto da divisao por 10 dnv
+# Ex: 123 // 10 =  12 INTEIROS, 12 / 10 = 1.2 ---> 1 dezena inteira com resto 2
+# logo o valor é o resto, 2
+
+centena = numeroPedido // 100
+#Divide o número por 100, pega o resultado inteiro
+#Ex: 123 // 100 = 1
+#logo, valor = 1
 
 
-inversao = str(primeiroNumero) + str(segundoNumero)
-
-print(inversao)
+numeroInvertido = unidade * 100 + dezena * 10 + centena
+print(f"Número invertido: {numeroInvertido}")
